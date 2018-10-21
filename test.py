f18da1a3856dec60aa2d8cb3967351e6499b9f27
birdbird1117 <dapeng.han.1989@gmail.com>
2018-10-21 14:05:30 +0200

    test sqllite

--- test.py
+++ test.py
@@ -6,3 +6,11 @@ cursor = connection.cursor()
 
 create_table = "CREATE TABLE users (id int, username text, password text)"
 cursor.execute(create_table)
+
+user = (1, 'jose', 'asdf')
+insert_query = "INSERT INTO users VALUES(?,?,?)"
+cursor.execute(insert_query, user)
+
+connection.commit()
+
+connection.close()
