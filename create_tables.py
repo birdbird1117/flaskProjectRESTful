0a7a5f004e3451d527f669e0199d3e4a564689e7
birdbird1117 <dapeng.han.1989@gmail.com>
2018-10-27 00:02:41 +0200

    retrieving items

--- create_tables.py
+++ create_tables.py
@@ -8,6 +8,13 @@ create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, userna
 
 cursor.execute(create_table)
 
+create_table = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
+
+cursor.execute(create_table)
+
+cursor.execute("INSERT INTO items VALUES ('test', 10.89)")
+
+
 connection.commit()
 
 connection.close()
