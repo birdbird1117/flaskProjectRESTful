43c2b2bdc271c9ec00ea877e6617e34fef0b3ce5
birdbird1117 <dapeng.han.1989@gmail.com>
2018-10-27 11:49:18 +0200

    create user item models

--- security.py
+++ security.py
@@ -1,13 +1,13 @@
 from werkzeug.security import safe_str_cmp
-from user import User
+from models.usermodel import UserModel
 
 
 def authenticate(username, password):
-    user = User.find_by_username(username)
+    user = UserModel.find_by_username(username)
     if user and safe_str_cmp(user.password, password):  # safer for different encoding cases, than string == string
         return user
 
 
 def identity(payload):
     userid = payload['identity']
-    return User.find_by_id(userid)
+    return UserModel.find_by_id(userid)
